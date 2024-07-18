3cf69b9b334bf50b3ca2a50ac0e66558b2b91b79
ranjana1999 <ranjanaranjithlal@gmail.com>
2024-07-19 00:34:53 +0530

    abc

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = "my-vm-instance"
+  name         = "my-vm-instance-new"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
