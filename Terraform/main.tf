ee466d72fb9189aadc5e64627116d5b90e03c1fe
ranjana1999 <ranjanaranjithlal@gmail.com>
2024-07-19 00:40:04 +0530

    abc

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,22 +1,4 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "my-vm-instance-new"  #new vm new
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  }
-
-}
 resource "google_compute_instance" "vm_instance" {
   name         = "my-vm-instance"
   machine_type = "e2-medium"
