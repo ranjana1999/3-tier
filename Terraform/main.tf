e557e8d09e8bc24131d9c1b15ea7d0d0df27b50d
ranjana1999 <ranjanaranjithlal@gmail.com>
2024-07-18 21:15:48 +0530

    first

---
+++ Terraform/main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project = "my-project-57732-v"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm-instance"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+}
